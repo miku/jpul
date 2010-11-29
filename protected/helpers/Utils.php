96746988e76e1e3e28e71ef8d059df156e80ef49
miku <martin.czygan@gmail.com>
2010-11-29 16:33:15 +0100

    minor

--- protected/helpers/Utils.php
+++ protected/helpers/Utils.php
@@ -21,7 +21,7 @@
 	{ 
 		if(strlen($string) > $length) 
 			return (preg_match('/^(.*)\W.*$/', 
-				substr($string, 0, $length+1), $matches) ? $matches[1] : substr($string, 0, $length)) . $replacer; 
+				mb_substr($string, 0, $length+1), $matches) ? $matches[1] : mb_substr($string, 0, $length)) . $replacer; 
 		return $string; 
 	}
 	

8c55c3f73a8b7a4bdef7a0b87e2073f751ef571b
miku <martin.czygan@gmail.com>
2010-11-29 16:43:30 +0100

    minor

--- protected/vendors/recaptcha-php-1.11/recaptchalib.php
+++ protected/vendors/recaptcha-php-1.11/recaptchalib.php
@@ -35,9 +35,9 @@
 /**
  * The reCAPTCHA server URL's
  */
-define("RECAPTCHA_API_SERVER", "http://www.google.com/recaptcha/api");
-define("RECAPTCHA_API_SECURE_SERVER", "https://www.google.com/recaptcha/api");
-define("RECAPTCHA_VERIFY_SERVER", "www.google.com");
+define("RECAPTCHA_API_SERVER", gethostbyname("http://www.google.com/recaptcha/api"));
+define("RECAPTCHA_API_SECURE_SERVER", gethostbyname("https://www.google.com/recaptcha/api"));
+define("RECAPTCHA_VERIFY_SERVER", gethostbyname("www.google.com"));
 
 /**
  * Encodes the given data into a query string format

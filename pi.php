29664a70ec68a5043e002ab84eca7fb12a45a8d8
miku <martin.czygan@gmail.com>
2010-11-29 16:40:46 +0100

    minor

--- pi.php
+++ pi.php
@@ -1,1 +1,8 @@
-<?php phpinfo(); ?>
+<!-- wont work -->
+<?php phpinfo(); ?> 
+
+<!-- wont work -->
+<?php echo gethostname(); ?>
+
+<!-- wont work? -->
+<?php echo gethostbyname('www.myyn.org'); ?>

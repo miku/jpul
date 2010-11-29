67a9ccaa395ff8a096979b65eed749a637e39335
miku <martin.czygan@gmail.com>
2010-11-29 16:30:16 +0100

    utils

--- protected/views/job/_teaser.php
+++ protected/views/job/_teaser.php
@@ -1,11 +1,11 @@
 <tr class="<?php if ($index % 2 == 0) { echo 'even'; } else { echo 'odd'; } ?>">
 	<td class="job-date-added"><?php echo strftime('%d.%m.%Y', $model->date_added) ?></td>		
 	<td class="job-title">
-		<a href="<?php echo $this->createUrl('job/view', array('id' => $model->id)); ?>" title="<?php echo $this->cut_text($model->description, 300) ?>">
-			<?php echo $this->cut_text($model->title, 40) ?>
+		<a href="<?php echo $this->createUrl('job/view', array('id' => $model->id)); ?>" title="<?php echo cut_text($model->description, 300) ?>">
+			<?php echo cut_text($model->title, 40) ?>
 		</a>
 	</td>
-	<td class="job-company"><?php echo $this->cut_text($model->company, 20) ?></td>
+	<td class="job-company"><?php echo cut_text($model->company, 20) ?></td>
 
 	<?php if (Yii::app()->user->isAdmin()): ?>
 		<?php if ($model->isExpired()): ?>

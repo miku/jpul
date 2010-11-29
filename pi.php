a2f5575b779d990a9625ff1a91c54b2e70eb48cc
miku <martin.czygan@gmail.com>
2010-11-29 18:20:59 +0100

    minor

--- pi.php
+++ pi.php
@@ -7,5 +7,12 @@
 <!-- works -->
 <?php // echo gethostbyname('www.myyn.org'); ?>
 
-<!-- works? -->
-<?php if (extension_loaded('gd') && function_exists('gd_info')) { echo "It looks like GD is installed"; } ?>
+<!-- works -->
+<?php // if (extension_loaded('gd') && function_exists('gd_info')) { echo "It looks like GD is installed"; } ?>
+
+<?php
+	$im = new imagick('uploads/example_file.pdf[0]');
+	$im->setImageFormat( "jpg" );
+	header( "Content-Type: image/jpeg" );
+	echo $im;
+?>

645799cd437fa52de2579272468f7207b66ba9f0
miku <martin.czygan@gmail.com>
2010-12-01 11:12:41 +0100

    minor

--- protected/views/layouts/framed.php
+++ protected/views/layouts/framed.php
@@ -19,7 +19,28 @@
 
 <div id="main-content">
 <?php echo $content; ?>
+
+
+<div style="border-top: solid thin gray; border-bottom: solid thin gray; margin-top: 60px; background: #EFEFEF; padding: 10px 10px 10px 10px;">
+					<p>
+					<a href="<?php echo $this->createUrl('job/index'); ?>">Job Listing</a><br>
+					<?php if (Yii::app()->user->getId() != NULL): ?>
+						<?php if (Yii::app()->user->isAdmin()): ?>
+							<a href="<?php echo $this->createUrl('job/create'); ?>">Neues Angebot einstellen</a><br>
+							<a href="<?php echo $this->createUrl('site/options'); ?>">Einstellungen</a><br>
+						<?php endif; ?>
+						Logged in as <a href="#"><?php echo Yii::app()->user->getUsername(); ?></a> | 
+						<a href="<?php echo $this->createUrl('site/logout'); ?>">Logout</a><br>					
+					<?php else: ?>
+						<a href="<?php echo $this->createUrl('job/draft'); ?>">Neues Angebot erstellen</a><br>
+						<a href="<?php echo $this->createUrl('site/login'); ?>">Login</a><br>
+					<?php endif; ?>
+
+				</p>
+
+</div>				
+
 </div>
-				
+
 </body>
 </html>

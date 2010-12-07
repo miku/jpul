bb35a0eebb369cffb3c623d870a976c2c6d4190e
miku <martin.czygan@gmail.com>
2010-12-07 11:21:43 +0100

    minor

--- protected/views/site/pages/employers.php
+++ protected/views/site/pages/employers.php
@@ -49,10 +49,6 @@
 		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/v2/cc_logo.gif" alt="" />
 		
 		<h1 class="spacetop">Für Arbeitgeber</h1>
-<p>Als Arbeitgeber haben Sie die Möglichkeit
-	Ausschreibung auf unserer Jobportal-Plattform
-	zu veröffentlichen. <a href="job/index?r=site/page&view=employers">Mehr lesen...</a>
-</p>
 <p>
 	<a href="<?php echo $this->createUrl('job/draft') ?>">Jetzt ein Angebot einstellen</a>
 </p>

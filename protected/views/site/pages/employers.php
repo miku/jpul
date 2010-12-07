dda6433e9db24dcb41ec16a9d5a4f07e21dd81dd
miku <martin.czygan@gmail.com>
2010-12-07 10:47:25 +0100

    minor

--- protected/views/site/pages/employers.php
+++ protected/views/site/pages/employers.php
@@ -6,44 +6,56 @@
 			Für Arbeitgeber und Institutionen
 		</div>
 
-		<div id="main-content" style="font-size: 12px; padding: 10px; margin: 10px;">
+		<div id="main-content" style="font-size: 12px; padding: 10px; margin: 10px 10px 10px 20px;">
 			
-<h2>Einstellung eines Angebotes im Jobportal des Career Centers</h2>
+<h2>Einstellung eines Angebotes im Jobportal des Career Centers</h2><br>
 
 <p>Im Folgenden zeigen wir Ihnen Schritt für Schritt, wie Sie Ihre Praktikums- oder Stellenanzeige im Jobportal des Career Centers selbst einpflegen können.</p>
+<br>
+
 
 1. Gehen Sie auf den Link Jetzt ein Angebot einstellen. Es erscheint die Eingabemaske für ein neues Angebot.<br>
-2. Es gibt lediglich fünf Pflichtfelder, die Sie ausfüllen müssen.<br>
-
+2. Es gibt lediglich fünf Pflichtfelder, die Sie ausfüllen müssen.<br><br>
+	
 <ul>
-<li>Titel (Jobtitel wie z.B. Controller, Lehrkraft, Praktikant(in), ...)</li>
-<li>Beschreibung (es handelt sich hierbei um ein Freitextfeld, in das Sie Voraussetzungen, Anforderungen, Einsatzdauer, Gehalt etc. eintragen können)</li>
-<li>Name des Unternehmens oder der Institution</li>
-<Li>Stadt bzw. Einsatzort des Angebotes</Li>
-<Li>Ablaufdatum bzw. Bewerbungsschluss der Anzeige im Jobportal</Li>
+<li><strong>Titel</strong> (Jobtitel wie z.B. Controller, Lehrkraft, Praktikant(in), ...)</li>
+<li><strong>Beschreibung</strong> (es handelt sich hierbei um ein Freitextfeld, in das Sie Voraussetzungen, Anforderungen, Einsatzdauer, Gehalt etc. eintragen können)</li>
+<li><strong>Name</strong> des Unternehmens oder der Institution</li>
+<Li><strong>Stadt</strong> bzw. Einsatzort des Angebotes</Li>
+<li><strong>Ablaufdatum</strong> bzw. Bewerbungsschluss der Anzeige im Jobportal</li>
 </ul>
+	
+	
+	
+</li>
+
+
+<br>
 
 <p>Alle weiteren Felder sind optional. Sie können diese ausfüllen, um den Studierenden mehr Information zu Ihrem Unternehmen oder Ihrer Institution zur Verfügung zu stellen (z.B. Angabe Ihrer Homepage).
-Zur Gestaltung Ihres Textes unter „Stellenbeschreibung“ haben Sie folgende Möglichkeiten:</p>
+Zur Gestaltung Ihres Textes unter „Stellenbeschreibung“ haben Sie folgende Möglichkeiten:</p><br>
 
-	I) *der Text zwischen zwei Sternen wird fett dargestellt* 	
-	II) _der Text zwischen zwei Unterstrichen wird kursiv dargestellt_ 	
-	III) Ihre Stichpunkte können Sie mit einem Stern (*) einleiten
+<ul>
+<li>*der Text zwischen zwei Sternen wird fett dargestellt* 	</li>
+<li>_der Text zwischen zwei Unterstrichen wird kursiv dargestellt_ 	</li>
+<li>Ihre Stichpunkte können Sie mit einem Stern (*) einleiten</li>
+</ul>
+
+<br>
 
 <p>Sie können außerdem Ihre persönlichen Kontaktdaten und Ihre E-Mail hinterlegen. Damit können wir Sie benachrichtigen, sobald Ihr Angebot freigeschaltet wurde.
 Sie haben auch die Möglichkeit, Ihre Anzeige als pdf-Datei hochzuladen.</p>
+
+<br>
 
 3. Nachdem Sie Ihre Angaben eingetragen haben, klicken Sie bitte auf „absenden“.<br>
 4. Ihre Anzeige wird von den Mitarbeiter(inne)n des Career Centers nach einer Prüfung freigeschaltet. Die Prüfung erfolgt lediglich auf Plausibilität und formale Fehler. Das Career Center behält sich allerdings vor, über eine endgültige Veröffentlichung zu entscheiden.<br>
 5. Wenn Sie eine Kontakt-E-Mail-Adresse angegeben haben, senden wir Ihnen eine Benachrichtigung über die Freischaltung Ihres Angebotes zu.<br>
 
+<br>
 <em>Falls Sie Fragen haben oder Hilfe beim Einstellen Ihres Angebotes benötigen, können Sie uns gern unter +49 341 97-30030 oder careercenter@uni-leipzig.de kontaktieren.</em>
 			
-			
-			
-			
-			
-			<a href="<?php echo Yii::app()->request->baseUrl; ?>/docs/Einstellung_eines_Angebotes_im_Jobportal_des_Career_Centers.pdf">Einstellung eines Angebotes im Jobportal des Career Centers</a> (PDF).
+<br><br>Diese Anleitung als PDF: <a href="<?php echo Yii::app()->request->baseUrl; ?>/docs/Einstellung_eines_Angebotes_im_Jobportal_des_Career_Centers.pdf">Einstellung eines Angebotes im Jobportal des Career Centers</a>.
 		</div>
 	</div>
 </div>

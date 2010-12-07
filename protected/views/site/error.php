e79f2f032c0f16de7d1a080aabe920aa2d50455b
miku <martin.czygan@gmail.com>
2010-12-07 09:53:43 +0100

    minor

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,12 +1,16 @@
-<?php
-$this->pageTitle=Yii::app()->name . ' - Error';
+<?php $this->pageTitle=Yii::app()->name . ' - Error'; ?>
 
-?>
+<div id="main">
+
+<div id="generic-header">
+<p>Login</p>	
+</div>
+
+<div id="main-content">
 
 <p>Diese Seite existiert nicht oder ist nicht verfügbar &mdash; HTTP Status <?php echo $code; ?>.</p>
 
 <a href="<?php echo $this->createUrl('job/index') ?>">Zur Homepage</a>
 
-<!-- <div class="error">
-<?php echo CHtml::encode($message); ?>
-</div> -->
+</div>	
+</div>

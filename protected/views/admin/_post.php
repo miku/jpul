d667ab2457fecb99362e2ba9811920454b1aead7
miku <martin.czygan@gmail.com>
2010-12-12 04:35:11 +0100

    pdf icon

--- protected/views/admin/_post.php
+++ protected/views/admin/_post.php
@@ -20,8 +20,8 @@
 		<?php echo format_model_location($model); ?>		
 	</p>
 	<p class="post-description-teaser"><?php echo cut_text($model->description, 100) ?></p>
-	<p class="small"><?php if ($model->attachment): ?>
-		Attachment: <a href="<?php echo $this->createUrl('job/download', array('id' => $model->id)); ?>"><?php echo $model->attachment ?></a>
+	<p class="post-attachment"><?php if ($model->attachment): ?>
+		<a class="pdf" href="<?php echo $this->createUrl('job/download', array('id' => $model->id)); ?>"><?php echo $model->attachment ?></a>
 	<?php endif ?></p>
 	<p></p>
 </div>

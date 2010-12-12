cbc4410b8a2e7b6f7a25f94b526d328ddfe163e6
miku <martin.czygan@gmail.com>
2010-12-12 04:38:08 +0100

    minor

--- protected/views/site/options.php
+++ protected/views/site/options.php
@@ -22,7 +22,7 @@
 			die eine Notifikation geschickt werden soll, falls ein neues Jobangebot von
 			einem Benutzer eingestellt wurde.
 		</td></tr>	
-		<tr class="even"><td><?php echo $form->textField($model,'value', array('size' => '70')); ?></td></tr>
+		<tr class="even"><td><?php echo $form->textField($model,'value', array('size' => '80')); ?></td></tr>
 		<tr class="even error"><td><?php echo $form->error($model,'option'); ?></td></tr>
 
 		<tr class="even-dark"><td><input type="submit" value="Speichern"> <span style="font-size:12px;">oder</span> <a style="font-size: 12px" href="<?php echo $this->createUrl('admin/index'); ?>">Abbrechen</a></td></tr>

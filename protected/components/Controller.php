3e5588330e4f8cc760a4b4f7ca786ff044dd12fe
miku <martin.czygan@gmail.com>
2010-12-15 01:48:40 +0100

    disabled synchronous request tracking

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -49,19 +49,18 @@ class Controller extends CController
 	}	
 	
 	protected function beforeAction($action) {
-		
-		
-		// simple request tracking ...
-		try {
-			$request = new Request();
-			$request->addr = $_SERVER["REMOTE_ADDR"];
-			$request->request_time = time();
-			$request->user_agent = $_SERVER["HTTP_USER_AGENT"];
-			$request->request_path = $_SERVER['REQUEST_URI'];
-			$request->save();
-		} catch (Exception $e) {
-			Yii::log("failed to record request: " . $e, CLogger::LEVEL_INFO, "beforeAction");
-		}
+				
+		// // simple request tracking ...
+		// try {
+		// 	$request = new Request();
+		// 	$request->addr = $_SERVER["REMOTE_ADDR"];
+		// 	$request->request_time = time();
+		// 	$request->user_agent = $_SERVER["HTTP_USER_AGENT"];
+		// 	$request->request_path = $_SERVER['REQUEST_URI'];
+		// 	$request->save();
+		// } catch (Exception $e) {
+		// 	Yii::log("failed to record request: " . $e, CLogger::LEVEL_INFO, "beforeAction");
+		// }
 		
 		CHtml::$afterRequiredLabel = '';
 		

4fc78412b6e278ab51f819e18c09433c6c07e372
miku <martin.czygan@gmail.com>
2011-02-11 12:27:47 +0100

    fixed location

--- protected/views/job/_sidebar_for_employer.php
+++ protected/views/job/_sidebar_for_employer.php
@@ -2,7 +2,7 @@
 <p>Als Arbeitgeber haben Sie die Möglichkeit,
 	Ausschreibungen auf unserer Jobportal-Plattform
 	zu veröffentlichen. Lesen Sie unsere
-	<a href="/docs/2011_02_07_Einstellung_eines_Angebotes_im_Jobportal.pdf">Anleitung als PDF</a>.
+	<a href="http://wwwdup.uni-leipzig.de/jobportal/docs/2011_02_07_Einstellung_eines_Angebotes_im_Jobportal.pdf">Anleitung als PDF</a>.
 </p>
 
 <script>

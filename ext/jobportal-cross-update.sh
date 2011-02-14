931f944aaa548626fadbe9e4703ae1b4721f8516
miku <martin.czygan@gmail.com>
2011-02-14 21:42:36 +0100

    more not working evil

--- ext/jobportal-cross-update.sh
+++ ext/jobportal-cross-update.sh
@@ -5,5 +5,4 @@ WORKTREE_ON_SSHFS=$HOME/tmp/ccul
 
 GIT_INDEX_FILE=$JOBPORTAL_HOME/.git/index \
 GIT_WORK_TREE=$WORKTREE_ON_SSHFS \
-# GIT_WORK_TREE=/sshfs-mount/on/production/server \
-	git reset --hard
+git reset --hard

7fabee9627e7fa6683007826da495dd67489bc49
miku <martin.czygan@gmail.com>
2011-03-02 13:37:36 +0100

    chronlogy

--- protected/views/stats/chronology.php
+++ protected/views/stats/chronology.php
@@ -18,10 +18,15 @@
 		<div id="main-content">
 			
 			<div class="col-by-total">
-			<?php $index = 0; ?>
+			<?php $index = 0 ?>
 			<?php foreach ($stats as $key => $value): ?>
 				<li <?php if ($index % 2 == 0): ?>style="background: aliceblue"<?php endif ?>><strong><?php echo $key; ?></strong>: <?php echo $value; ?></li>
 				<?php $index += 1; ?>
+				
+				<?php if ($index % 3 == 0): ?>
+					<div style="border-bottom: solid thin black; margin: 5px"></div>
+				<?php endif ?>
+				
 			<?php endforeach ?>
 			</div>
 

9e9cec8e90c052d70563d4444978d0ca3b2e7b15
miku <martin.czygan@gmail.com>
2011-04-21 18:13:29 +0200

    more languages in example search

--- protected/views/job/index.php
+++ protected/views/job/index.php
@@ -118,8 +118,10 @@
 		</p>
 		<br>
 		<p><strong>Beispielsuchen</strong> &middot;
-			<a href="<?php echo $this->createUrl('job/index', array("q" => "english OR englisch ")); ?>">Englischkenntnisse</a> &middot;
-			<a href="<?php echo $this->createUrl('job/index', array("q" => "french OR spanish OR chinese OR französisch OR spanisch OR russisch OR chinesisch")); ?>">Sprachen außer Englisch</a> &middot;
+			<a href="<?php echo $this->createUrl('job/index', array("q" => "english OR englisch")); ?>">Englischkenntnisse</a> &middot;
+			<a href="<?php echo $this->createUrl('job/index', 
+				array("q" => 
+					"french OR französisch OR spanish OR spanisch OR chinese OR chinesisch OR russian OR russisch OR polish OR polnisch OR czech OR tschechisch OR serbian OR serbisch OR italian OR italienisch OR hungarian OR ungarisch OR swedish OR schwedisch OR norwegian OR norwegisch OR finnish OR finnisch OR estonian OR estnisch OR bulgarian OR bulgarisch OR romanian OR rumänisch OR ukrainian OR ukrainisch")); ?>">Sprachen außer Englisch</a> &middot;
 			<a href="<?php echo urldecode($this->createUrl('job/index', array("q" => "übersetz*+OR+translate+OR+translation"))); ?>">Übersetzung</a> &middot;
 			<a href="<?php echo $this->createUrl('job/index', array("q" => "public relation")); ?>">PR Jobs</a> &middot;
 			<a href="<?php echo $this->createUrl('job/index', array("q" => "sport")); ?>">Jobs im Sport</a> &middot;

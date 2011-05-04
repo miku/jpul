5e0188b5f99d97354bd3b1b357932c0723670a32
miku <martin.czygan@gmail.com>
2011-05-05 00:17:04 +0200

    new fb

--- protected/views/shared/_sidebar_fb.php
+++ protected/views/shared/_sidebar_fb.php
@@ -1,6 +1,8 @@
 <h1 class="spacetop">Career Center @ Facebook</h1>
 <p>
-	<a href="http://www.facebook.com/home.php#!/pages/Universitat-Leipzig-Career-Center/193457397331303" title="Career Center Universität Leipzig bei Facebook (Link öffnet neues Fenster)" target="_blank" >
-		<img style="border: 0px none;" src="<?php echo Yii::app()->request->baseUrl; ?>/images/v2/Facebook_Icon_klein.png" alt="" />
+	<a href="http://www.facebook.com/universitaet.leipzig.career.center" title="Career Center Universität Leipzig bei Facebook (Link öffnet neues Fenster)" target="_blank" >
+		<!-- <img style="border: 0px none;" src="<?php echo Yii::app()->request->baseUrl; ?>/images/v2/Facebook_Icon_klein.png" alt="" /> -->
+		<img style="border: 0px none;" src="<?php echo Yii::app()->request->baseUrl; ?>/images/v2/facebook_logo_75x.jpg" alt="" />
+		
 	</a>
 </p>

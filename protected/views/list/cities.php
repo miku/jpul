1dc0b53d3365abe55a082d9ae3759555bbe9513f
miku <martin.czygan@gmail.com>
2011-05-04 20:39:54 +0200

    compact listings

--- protected/views/list/cities.php
+++ protected/views/list/cities.php
@@ -15,9 +15,8 @@
 
 <div id="main-container">
 <div id="main">
-	<div id="main-header">
+	<div id="generic-header">
 			<p>Angebote aus <?php echo count($dataReader); ?> Regionen</p>
-			<br>
 			<p class="small">Sortieren 
 				<a href="<?php echo $this->createUrl('list/cities', array('sort' => 'name')); ?>">nach Name</a>, 
 				<a href="<?php echo $this->createUrl('list/cities', array('sort' => 'count')); ?>">nach Anzahl der Angebote</a></p>
@@ -28,7 +27,9 @@
 
 <div class="col-by-total">
 <?php foreach ($dataReader as $key => $value): ?>
-	<li><a href="<?php echo urldecode($this->createUrl('job/index', array('q' => $value["city"] ))); ?>"><?php echo $value["city"] ?></a> (~ <?php echo $value["total"] ?>)</li>
+	<li style="padding: 3px 0 3px 10px; margin: 0;"><a href="<?php echo urldecode($this->createUrl('job/index', array('q' => $value["city"] ))); ?>"><?php echo $value["city"] ?></a> 
+		<span style="font-size: 9px">(~ <?php echo $value["total"] ?> Angebote)</span>
+	</li>
 <?php endforeach ?>
 </div>
 
--- protected/views/list/companies.php
+++ protected/views/list/companies.php
@@ -16,9 +16,8 @@
 
 <div id="main-container">
 <div id="main">
-	<div id="main-header">
-			<p><?php echo count($dataReader); ?> Unternehmen und Institutionen haben aktuelle Angebote auf unserem Portal veröffentlicht.</p>
-			<br>
+	<div id="generic-header">
+			<p><?php echo count($dataReader); ?> Unternehmen und Institutionen haben aktuelle Angebote auf unserem Portal veröffentlicht</p>
 			<p class="small">Sortieren 
 				<a href="<?php echo $this->createUrl('list/companies', array('sort' => 'name')); ?>">nach Name</a>, 
 				<a href="<?php echo $this->createUrl('list/companies', array('sort' => 'count')); ?>">nach Anzahl der Angebote</a></p>
@@ -30,7 +29,9 @@
 <div class="col-by-total">
 <?php foreach ($dataReader as $key => $value): ?>
 	<!-- <li><a href="<?php echo $this->createUrl('job/index', array('q' => preg_replace('/\(|\)|\./', ' ', $value["company"]))); ?>"><?php echo $value["company"] ?></a> (<?php echo $value["total"] ?>)</li> -->
-	<li><a href="<?php echo urldecode($this->createUrl('job/index', array('q' => 'cc:' . purify($value["company"], '') ))); ?>"><?php echo $value["company"] ?></a> (~ <?php echo $value["total"] ?>)</li>
+	<li style="padding: 3px 0 3px 10px; margin: 0;"><a href="<?php echo urldecode($this->createUrl('job/index', array('q' => 'cc:' . purify($value["company"], '') ))); ?>"><?php echo $value["company"] ?></a>
+		<span style="font-size: 9px">(~ <?php echo $value["total"] ?> Angebot<?php if ($value["total"] > 1) { echo "e"; } ?>)</span>
+	</li>
 <?php endforeach ?>
 </div>
 

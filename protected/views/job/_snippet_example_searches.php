42d687961f68069f32e267db8edcf350e53d74ec
miku <martin.czygan@gmail.com>
2011-05-17 01:55:19 +0200

    add shk shortcut

--- protected/views/job/_snippet_example_searches.php
+++ protected/views/job/_snippet_example_searches.php
@@ -1,4 +1,5 @@
 <a href="<?php echo $this->createUrl('job/index', array('q' => "marketing")) ?>">marketing</a>,
 <a href="<?php echo $this->createUrl('job/index', array('q' => "wissenschaft")) ?>">wissenschaft</a>,
+<a href="<?php echo $this->createUrl('job/index', array('q' => "shk")) ?>">shk</a>,
 <a href="<?php echo $this->createUrl('job/index', array('q' => "leipzig")) ?>">leipzig</a>, 
 <a href="<?php echo urldecode($this->createUrl('job/index', array('q' => "praktik*"))) ?>">praktik*</a>, etc.

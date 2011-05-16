74615701b667d12ec0463720136e54f6fa0257c6
miku <martin.czygan@gmail.com>
2011-05-17 01:56:57 +0200

    minor

--- protected/views/job/_snippet_example_searches.php
+++ protected/views/job/_snippet_example_searches.php
@@ -2,4 +2,4 @@
 <a href="<?php echo $this->createUrl('job/index', array('q' => "wissenschaft")) ?>">wissenschaft</a>,
 <a href="<?php echo $this->createUrl('job/index', array('q' => "shk")) ?>">shk</a>,
 <a href="<?php echo $this->createUrl('job/index', array('q' => "leipzig")) ?>">leipzig</a>, 
-<a href="<?php echo urldecode($this->createUrl('job/index', array('q' => "praktik*"))) ?>">praktik*</a>, etc.
+<a href="<?php echo urldecode($this->createUrl('job/index', array('q' => "praktik*"))) ?>">praktik*</a>, ...

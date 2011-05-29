28ffc35923c6f84114c3c834564f800e0b9f4338
miku <martin.czygan@gmail.com>
2011-05-30 00:42:25 +0200

    disable caching

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -29,17 +29,19 @@ class Controller extends CController
 	
 	// The number of items per page (only 10, 20 or 50 as possible at the moment!)
 	public $items_per_page = 10;
+
+	// Caching the complete listings
 	
-	public function filters()
-	{
-    	return array(
-        	array(
-            	'COutputCache',
-            	'duration'=>3,
-            	'varyByParam'=>array('id', 'f', 'q', 'size', 'l', 'page'),
-        	),
-    	);
-	}
+	// public function filters()
+	// {
+	//     	return array(
+	//         	array(
+	//             	'COutputCache',
+	//             	'duration'=>2,
+	//             	'varyByParam'=>array('id', 'q', 'size', 'cache', 'page'),
+	//         	),
+	//     	);
+	// }
 	
 	
 	/**

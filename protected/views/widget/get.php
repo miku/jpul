2c0a594125b63dd9a04f012113a80691c7091804
miku <martin.czygan@gmail.com>
2011-06-01 02:48:56 +0200

    longer title

--- protected/views/widget/get.php
+++ protected/views/widget/get.php
@@ -52,7 +52,7 @@
 <ul style="">
 	<?php foreach ($models as $index => $model): ?>
 	<li style="<?php if ($index % 2 == 0) { echo 'background: aliceblue'; } ?>"><a target="_blank" style="" href="http://wwwdup.uni-leipzig.de<?php echo $this->createUrl('job/view', array('id' => $model->id, 'src' => 'widget')); ?>">
-		<?php echo cut_text($model->title, 40); ?></a> <span class="ccul_date" style="">
+		<?php echo cut_text($model->title, 60); ?></a> <span class="ccul_date" style="">
 		<?php echo strftime("%d.%m.%Y", $model->date_added); ?></span></li>
 	<?php endforeach ?>
 </ul>

82bdbb6b8e1ae7a3974b5f2597a27b72439c14df
miku <martin.czygan@gmail.com>
2011-06-21 22:15:39 +0200

    added migration for options; not used right now

---
+++ migrations/018-alter-options-table.sql
@@ -0,0 +1,20 @@
+START TRANSACTION;
+
+ALTER TABLE `options` ADD COLUMN `type` VARCHAR(32) default NULL;
+ALTER TABLE `options` ADD COLUMN `help` VARCHAR(1024) default NULL;
+
+INSERT INTO schema_version(`migration_code`, `extra_notes`) VALUES (
+	'018', 'added type and help columns to options; type should indicate the type of the value, e.g. string or boolean, which is eventually used to render the form; help is just a short description');
+
+COMMIT;
+
+-- Update existing record
+
+START TRANSACTION;
+
+UPDATE `options` SET 
+	type = 'string', 
+	help = 'Kommaseparierte Liste von E-Mail-Addressen, an die eine Notifikation gesendet werden soll, wenn ein neues Angebot von einem Arbeitgeber eingestellt wurde.' 
+	WHERE id = 1;
+
+COMMIT;

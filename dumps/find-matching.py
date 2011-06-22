d6f974bdc888ccb8c8a7707ecd4c43ebd4bcb707
miku <martin.czygan@gmail.com>
2011-06-22 23:33:02 +0200

    find matching tabs to spaces

--- dumps/find-matching.py
+++ dumps/find-matching.py
@@ -14,29 +14,29 @@ FS_DUMP_PREFIX = 'webdir'
 DB_DUMP_PREFIX = 'jobportal'
 
 def find_closest(prefix, target_ts):
-	closest_fn, distance = None, sys.maxint
-	for fn in filter(lambda x: x.startswith(prefix), os.listdir('.')):
-		try:
-			timestamp = re.search("\d{10,10}", fn).group()
-		except AttributeError:
-			continue
-		diff = abs(int(target_ts) - int(timestamp))
-		if diff < distance:
-			closest_fn, distance = fn, diff
-	print closest_fn.ljust(35), '%.2f' % (distance / 60), 'minute(s) away'
+    closest_fn, distance = 'Not found', sys.maxint
+    for fn in filter(lambda x: x.startswith(prefix), os.listdir('.')):
+        try:
+            timestamp = re.search("\d{10,10}", fn).group()
+        except AttributeError:
+            continue
+        diff = abs(int(target_ts) - int(timestamp))
+        if diff < distance:
+            closest_fn, distance = fn, diff
+    print closest_fn.ljust(35), '%.2f' % (distance / 60), 'minute(s) away'
 
 def main(filename):
-	""" Extract 1303313960 like string from filename"""
-	timestamp = re.search("\d{10,10}", filename).group()
-	if not timestamp:
-		raise Exception("No file matching \d{10,10}.")
-	print 'Target: %s (%s)\n' % (timestamp, datetime.datetime.fromtimestamp(int(timestamp)).isoformat())
-	find_closest(FS_DUMP_PREFIX, timestamp)
-	find_closest(DB_DUMP_PREFIX, timestamp)
+    """ Extract 1303313960 like string from filename"""
+    timestamp = re.search("\d{10,10}", filename).group()
+    if not timestamp:
+        raise Exception("No file matching \d{10,10}.")
+    print 'Target: %s (%s)\n' % (timestamp, datetime.datetime.fromtimestamp(int(timestamp)).isoformat())
+    find_closest(FS_DUMP_PREFIX, timestamp)
+    find_closest(DB_DUMP_PREFIX, timestamp)
 
 if __name__ == '__main__':
-	try:
-		main(sys.argv[1])
-	except IndexError:
-		print 'Find matching DB and FS dump files.'
-		print 'Usage: %s [FILENAME]' % (os.path.basename(sys.argv[0]))
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        print 'Find matching DB and FS dump files.'
+        print 'Usage: %s [FILENAME]' % (os.path.basename(sys.argv[0]))

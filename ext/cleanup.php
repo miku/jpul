ab45c018c1e349a7e0794b46140b4999cddabd8c
miku <martin.czygan@gmail.com>
2011-06-22 18:05:03 +0200

    cleanup script

---
+++ ext/cleanup.php
@@ -0,0 +1,18 @@
+<?php
+
+header("Content-Type: text/plain");
+
+$directory = '../protected/runtime/cache';
+	
+if ($handle = opendir($directory)) {
+    while (false !== ($file = readdir($handle))) {
+        if ($file != "." && $file != "..") {
+			$filepath = $directory . '/' . $file;
+            echo $filepath . "\n";
+			unlink($filepath);
+        }
+    }
+    closedir($handle);
+}	
+
+?>

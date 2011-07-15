8bdf1cba2f5664f09fb2ad8c82cba9e80d46617d
miku <martin.czygan@gmail.com>
2011-07-15 14:22:09 +0200

    fast widget search

--- protected/controllers/WidgetController.php
+++ protected/controllers/WidgetController.php
@@ -3,6 +3,8 @@
 // Textile via Utils.php
 Yii::import('application.helpers.*');
 require_once('Utils.php');
+require_once('SearchHub.php');
+
 
 class WidgetController extends Controller
 {
@@ -20,60 +22,57 @@ class WidgetController extends Controller
 			array('original_query' => $original_query, 'width' => $width));
 	}
 	
-	public function actionGet() {
+	public function actionGet($tab = 'all', $limit = 10) {
 		
-		Yii::log("Widget-Request from " . isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : '', 
-			CLogger::LEVEL_INFO, __FUNCTION__);
-		
-		Zend_Search_Lucene_Analysis_Analyzer::setDefault(
-    		new Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8_CaseInsensitive());
-		Zend_Search_Lucene_Search_QueryParser::setDefaultEncoding('utf-8');
+		Yii::log("Widget-Request from " . isset($_SERVER["REMOTE_ADDR"]) 
+			? $_SERVER["REMOTE_ADDR"] : '', CLogger::LEVEL_INFO, __FUNCTION__);
 
 		$current_time = time();
-		$criteria = new CDbCriteria;
-		$criteria->order = 'date_added DESC';
-	
-		$criteria->condition = 'status_id=:status_id AND expiration_date > :current_time';
-		$criteria->params=array(':status_id' => 2, ':current_time' => $current_time);
-		$criteria->limit = 10; # self::PAGE_SIZE;
 
 		// Determine the view to use ...
 		$viewName = "default";
 		$original_query = '';
+		
+		if (!is_numeric($limit)) {
+			$limit = 10;
+		} elseif ($limit > 50 || $limit < 3) {
+			$limit = 10;
+		}
 
 		if (isset($_GET['q']) && $_GET['q'] != '') {
 			$viewName = "search";
 		}
 
 		if ($viewName == "search") {
-			$index = new Zend_Search_Lucene($this->getSearchIndexStore());
 			$original_query = $_GET['q'];
 
+			// If the user does not use anything from the extended search
+			// syntax, append kleene star to terms
 			if (preg_match("/( OR | AND |\"|:|~|-|\*| NOT )/", $original_query) == 0) {
-				$query = trim($original_query) . '*';
-				$query = preg_replace("/\s+/", "* AND ", $query);
+			    $query = trim($original_query) . '*';
+			    $query = preg_replace("/\s+/", "* ", $query);
 			} else {
-				$query = $original_query;
-			}
-			
-			Yii::log("Q: " . $query, CLogger::LEVEL_INFO, __FUNCTION__);
-			
-			try {
-				$results = $index->find($query);
-			} catch (Exception $e) {
-				Yii::log("Failed Query: '" . $query . "' - Exception: " . $e, CLogger::LEVEL_INFO, __FUNCTION__);
-				$this->redirect(array('index'));
+			    $query = $original_query;
 			}
 
-			$pks = array();
- 			foreach ($results as $result) {
-				$pks[] = $result->pk;
-			}
-			$models = Job::model()->findAllByAttributes(array('id' => $pks), $criteria);
-		}	
-		
-		// if the user neither searched or requested her favs, use the default view ...	
-		if ($viewName == "default") {
+			// Correct the user input to the query we are actually executing.
+			$original_query = $query;
+
+			$options = array("q" => $query, "tab" => $tab);
+			$options["offset"] = 0;
+            $options["limit"] = $limit;
+
+			$result = getResultSetAndSize($options);
+			$models = $result["models"];
+
+		} elseif ($viewName == "default") {
+
+			$criteria = new CDbCriteria;
+			$criteria->order = 'date_added DESC';	
+			$criteria->condition = 'status_id=:status_id AND expiration_date > :current_time';
+			$criteria->params=array(':status_id' => 2, ':current_time' => $current_time);
+			$criteria->limit = $limit;
+			
 			$models = Job::model()->findAll($criteria);
 		}
 

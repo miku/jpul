f0e7797f21bc7e465ca67eafab06e06d12cdd494
miku <martin.czygan@gmail.com>
2011-07-17 15:57:29 +0200

    tabs to spaces

--- t-dirsize.php
+++ t-dirsize.php
@@ -1,11 +1,11 @@
 <?php
-	header("Content-Type: text/plain");
+    header("Content-Type: text/plain");
     $f = '.';
     $io = popen ( '/usr/bin/du -ks ' . $f, 'r' );
     $output = fgets ( $io, 4096);
     $result = preg_split('/\s/', $output);
-	$size = $result[0];
+    $size = $result[0];
     pclose ( $io );
-	// du output in kB 
+    // du output in kB 
     echo $f . ' => ' . $size / 1048576;
 ?>

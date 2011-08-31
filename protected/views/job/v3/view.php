69c9baad64d4eb1728f2f36077eb45e652a445cb
Martin Czygan <martin.czygan@gmail.com>
2011-08-31 15:52:44 +0200

    gray out expired offers

--- protected/views/job/v3/view.php
+++ protected/views/job/v3/view.php
@@ -1,7 +1,19 @@
-<!-- jv:1 -->
+<!-- jv:3 -->
+<style type="text/css" media="screen">
+    .expiration-wrapper {
+    	opacity : 0.4;
+    	filter: alpha(opacity=40); // msie
+    	background-color: #555;
+    }
+</style>
 <div id="main-container">
     <div id="main">
-        <div id="view-content">
+
+        <?php if ($model->expiration_date < time()): ?>
+            <p style="text-align: center; background: #EFEFEF; padding: 10px; font-size: 12px;">Der Bewerbungsschluss für dieses Angebot endete <?php echo time_since($model->expiration_date) ?>.</p>
+        <?php endif ?>
+
+        <div id="view-content" <?php if ($model->expiration_date < time()): ?>class="expiration-wrapper"<?php endif ?>>
 
             <div id="view-header">
 
@@ -9,7 +21,7 @@
 
                 <div id="mini-nav">
                     <?php
-                        $kvlist = explode('&', urldecode(Yii::app()->request->queryString)); 
+                        $kvlist = explode('&', urldecode(Yii::app()->request->queryString));
                         $params = array("page" => 1);
                         foreach ($kvlist as $index => $kvitem) {
                             $kv = explode('=', $kvitem);
@@ -18,13 +30,13 @@
                             }
                         }
                     ?>
-                    
+
                     <?php if (isset($params['src']) && $params['src'] == 'widget'): ?>
                         <a href="<?php echo $this->createUrl('job/index'); ?>">Zurück zur Übersicht</a>
                     <?php else: ?>
                         <a href="javascript:history.go(-1)">Zurück zur Übersicht</a>
                     <?php endif ?>
-                    
+
                     | <a href="<?php echo $this->createUrl('job/print', array('id' => $model->id)); ?>">Druckansicht</a>
                     <script type="text/javascript" charset="utf-8">
                         $(document).ready(function(){
@@ -36,23 +48,23 @@
                             });
                         });
                     </script>
-                    
+
                     <span id="more-from-company" style="display:none">| Mehr Angebote von <a href="<?php echo urldecode($this->createUrl('job/index', array('q' => 'cc:' . purify($model->company, ''), 'src' => 'more-from-company' ))); ?>"><?php echo cut_text($model->company, 30); ?></a></span>
                 </div>
 
-                
+
                 <div id="view-job-title"><?php echo $model->title ?></div>
 
                 <div id="view-job-subtitle">
-                    Eingestellt am <?php echo strftime('%d.%m.%Y', $model->date_added) ?>, 
-                    von 
+                    Eingestellt am <?php echo strftime('%d.%m.%Y', $model->date_added) ?>,
+                    von
                     <?php if ($model->company_homepage): ?>
                         <span class="view-job-company"><a href="<?php echo $model->company_homepage ?>"><?php echo $model->company ?></a></span>.
                     <?php else: ?>
                         <span class="view-job-company"><?php echo $model->company ?></span>.
                     <?php endif; ?>
-                    <?php echo format_model_location($model); ?>    
-                </div>      
+                    <?php echo format_model_location($model); ?>
+                </div>
             </div>
 
 <?php $this->endCache(); } ?>
@@ -64,7 +76,7 @@
                     <p><strong>Abschluß:</strong> <?php echo $model->degree->name ?></p>
                 <?php endif ?>
             </div>
-            
+
             <?php if ($model->attachment): ?>
                 <div id="view-download">
                     <a href="<?php echo $this->createUrl('job/download', array('id'=>$model->id)); ?>">PDF dieser Anzeige</a>
@@ -86,70 +98,70 @@
             </div>
 
 <?php $this->endCache(); } ?>
-            
+
             <?php if (isset($view_count)): ?>
             <div id="view-count">
-                <p>Dieses Angebot wurde 
-                    <?php echo ($view_count == 0) ? 1 : $view_count; ?> 
+                <p>Dieses Angebot wurde
+                    <?php echo ($view_count == 0) ? 1 : $view_count; ?>
                 mal angesehen.</p>
-            </div>              
+            </div>
             <?php endif ?>
-            
-            <div id="bottom-nav">   
-                
-				<a href="javascript:history.go(-1)">Zurück zur Übersicht</a>
-				
-				<?php if (isset(Yii::app()->session["idlist"]) && 
-					Yii::app()->session["idlist"] != null && 
-					count(Yii::app()->session["idlist"]) > 0): ?>
-					<span class="alignright" style="color: #767676">Tip: Mit <strong>&larr;</strong> und <strong>&rarr;</strong> können Sie in ihren Suchergebnissen navigieren.</span>					
-				<?php endif ?>
+
+            <div id="bottom-nav">
+
+                <a href="javascript:history.go(-1)">Zurück zur Übersicht</a>
+
+                <?php if (isset(Yii::app()->session["idlist"]) &&
+                    Yii::app()->session["idlist"] != null &&
+                    count(Yii::app()->session["idlist"]) > 0): ?>
+                    <span class="alignright" style="color: #767676">Tip: Mit <strong>&larr;</strong> und <strong>&rarr;</strong> können Sie in ihren Suchergebnissen navigieren.</span>
+                <?php endif ?>
 
             </div>
-                
-        </div>  
+
+        </div>
     </div>
 </div>
 
 <div id="sidebar-container">
     <div id="sidebar">
-        
+
         <?php if($this->beginCache("job_view_sidebar", array('duration'=>86400))) { ?>
-        
+
             <?php $this->renderPartial('/shared/_sidebar_contact'); ?>
             <?php $this->renderPartial('/shared/_sidebar_feedback'); ?>
             <?php $this->renderPartial('/shared/_sidebar_for_employer'); ?>
             <?php $this->renderPartial('/shared/_sidebar_fb'); ?>
             <?php $this->renderPartial('/shared/_sidebar_supporter'); ?>
-        
+
         <?php $this->endCache(); } ?>
 
         <?php if (Yii::app()->user->isAdmin()): ?>
             <h1 class="spacetop">Admin</h1>
             <p><a href="<?php echo $this->createUrl('admin/update', array('id' => $model->id)) ?>">Dieses Angebot bearbeiten</a></p>
         <?php endif ?>
-        
-    </div>  
+
+    </div>
 </div>
 
 <script type="text/javascript" charset="utf-8">
-	$(document).ready(function(){
-		key("right, j, l", function(){
-			$("#flash-message").html("Nächstes Angebot...");
-			$.get("<?php echo $this->createUrl('job/nextId'); ?>?c=<?php echo $model->id ?>", function(data) {
-				if (data == -1) { $("#flash-message").html(""); return false; }
-				var url = "<?php echo $this->createUrl('job/view', array('id' => '__ID__', 'src' => 'kb')); ?>".replace('__ID__', data);
-				window.location.replace(url);
-			});
-		});
-		key("left, h, k", function(){ 
-			$("#flash-message").html("Vorheriges Angebot...");
-			$.get("<?php echo $this->createUrl('job/previousId'); ?>?c=<?php echo $model->id ?>", function(data) {
-				if (data == -1) { $("#flash-message").html(""); return false; }
-				var url = "<?php echo $this->createUrl('job/view', array('id' => '__ID__', 'src' => 'kb')); ?>".replace('__ID__', data);
-				window.location.replace(url);
-			});
-		});
-		key("esc, q, z", function(){ $("#flash-message").html("Übersicht..."); history.go(-1); });
-	});
+    $(document).ready(function(){
+        key("right, j, l", function(){
+            $("#flash-message").html("Nächstes Angebot...");
+            $.get("<?php echo $this->createUrl('job/nextId'); ?>?c=<?php echo $model->id ?>", function(data) {
+                if (data == -1) { $("#flash-message").html(""); return false; }
+                var url = "<?php echo $this->createUrl('job/view', array('id' => '__ID__', 'src' => 'kb')); ?>".replace('__ID__', data);
+                window.location.replace(url);
+            });
+        });
+        key("left, h, k", function(){
+            $("#flash-message").html("Vorheriges Angebot...");
+            $.get("<?php echo $this->createUrl('job/previousId'); ?>?c=<?php echo $model->id ?>", function(data) {
+                if (data == -1) { $("#flash-message").html(""); return false; }
+                var url = "<?php echo $this->createUrl('job/view', array('id' => '__ID__', 'src' => 'kb')); ?>".replace('__ID__', data);
+                window.location.replace(url);
+            });
+        });
+        key("esc, q, z", function(){ $("#flash-message").html("Übersicht..."); history.go(-1); });
+    });
 </script>
